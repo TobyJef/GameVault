3ce4ba9d7848ce164ce26cc35c1f984ac63ea76a
Toby Jefferies <toby_jefferies@hotmail.com>
2024-09-11 21:18:23 +0000

    add csrf trusted origin token to fix bug

--- gamevault/settings.py
+++ gamevault/settings.py
@@ -1,3 +1,7 @@
+import os
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-tobyjef-gamevault-hamkc7nbjjo.ws-eu116.gitpod.io']
+
 """
 Django settings for gamevault project.
 
@@ -82,6 +86,16 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'gamevault.wsgi.application'
 
