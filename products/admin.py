85dd9a752651f31aecca7078b99a7a1299994817
Toby Jefferies <toby_jefferies@hotmail.com>
2024-09-24 14:17:05 +0000

    added products views and templates

--- products/admin.py
+++ products/admin.py
@@ -5,14 +5,14 @@ from .models import Product, Category
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
-
+        'name',
         'category',
         'price',
         'rating',
         'image',
-
     )
 
+    ordering = ('sku',)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
--- products/models.py
+++ products/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     colour = models.CharField(max_length=50, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
