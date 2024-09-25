3dad44e919dc990fd4fa13157bd651e72d01f66e
Toby Jefferies <toby_jefferies@hotmail.com>
2024-09-25 15:11:26 +0000

    added accessories to product database

--- products/models.py
+++ products/models.py
@@ -25,7 +25,8 @@ class Product(models.Model):
     storage = models.CharField(max_length=10, null=True, blank=True)
     colour = models.CharField(max_length=50, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    age = models.CharField(max_length=10, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
