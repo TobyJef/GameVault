d7227c43ee3a9e406bb2dfeabc3d6e6395a796ba
Toby Jefferies <toby_jefferies@hotmail.com>
2024-09-27 16:09:45 +0000

    begin new model construction for games and consoles

--- products/models.py
+++ products/models.py
@@ -34,3 +34,30 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Game(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    genre = models.CharField(max_length=100)
+    required_storage = models.CharField(max_length=10, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    players = models.CharField(max_length=10, null=True, blank=True)
+    age = models.CharField(max_length=10, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+
+
+class Console(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    internal_storage = models.CharField(max_length=10, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
