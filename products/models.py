1a48541c109354103c1ef8e5a87e3afaebbb2409
Toby Jefferies <toby_jefferies@hotmail.com>
2024-09-27 21:31:11 +0000

    update products model and add sorting and products count to products page

--- products/models.py
+++ products/models.py
@@ -22,13 +22,14 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    genre = models.CharField(max_length=100, null=True, blank=True)
     internal_storage = models.CharField(max_length=10, null=True, blank=True)
     required_storage = models.CharField(max_length=10, null=True, blank=True)
     colour = models.CharField(max_length=50, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     players = models.CharField(max_length=10, null=True, blank=True)
-    age = models.CharField(max_length=10, null=True, blank=True)
-    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    age_rating = models.CharField(max_length=10, null=True, blank=True)
+    star_rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
@@ -40,14 +41,13 @@ class Game(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    genre = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100, null=True, blank=True)
     required_storage = models.CharField(max_length=10, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     players = models.CharField(max_length=10, null=True, blank=True)
-    age = models.CharField(max_length=10, null=True, blank=True)
-    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    age_rating = models.CharField(max_length=10, null=True, blank=True)
+    star_rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+
 
 
 
@@ -58,6 +58,4 @@ class Console(models.Model):
     description = models.TextField()
     internal_storage = models.CharField(max_length=10, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    star_rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
