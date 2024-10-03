3565b7e8b6aea669bf64c3f822d38951c000e49a
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-03 23:07:37 +0000

    added signals.py and forms.py files and added post save post delete signals

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'grand_total')
+    
+    fields = ('order_number', 'date', 'first_name', 'last_name', 'email', 'contact_number',
+              'country', 'post_zipcode', 'town_or_city', 'street_address_1', 'street_address_2',
+              'county_or_state', 'delivery_cost', 'order_total', 'grand_total')
+
+    list_display = ('order_number', 'date', 'first_name', 'last_name', 'delivery_cost', 'order_total', 'grand_total')
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
