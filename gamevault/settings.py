9526891d221c870fc142c4cc9ce8bdc5b2167b6c
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-04 22:05:04 +0000

    added crispy-forms builtins to settings.py and updated requirements.txt

--- gamevault/settings.py
+++ gamevault/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'products',
     'basket',
     'checkout',
+
+    #Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gamevault.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -81,6 +86,11 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'basket.contexts.basket_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags'
+                'crispy_forms.templatetags.crispy_forms_field'
+
+            ]
         },
     },
 ]
