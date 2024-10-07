bf22fcf92008263d85d0f8256d806327f3e305bd
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-07 15:07:15 +0000

    Product Admin - added front end Delete Product capability

--- products/views.py
+++ products/views.py
@@ -72,9 +72,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=['product.id']))
         else:
             messages.error(request, 'Failed to add product. Please ensure all forms are complete and valid')
     else:
@@ -110,19 +110,8 @@ def edit_product(request, product_id):
 
 
 def delete_product(request, product_id):
-    """ Edit a product in the store """
+    """ Delete a product in the store """
     product = get_object_or_404(Product, pk=product_id)
-    if request.method == 'POST':
-        form = ProductForm(instance=product)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Product Successfully Removed')
-    
-    form = ProductForm(instance=product)
-    messages.info(request, f'You are deleting {product.name}')
-
-    template = 'products/delete_product.html'
-    context = {
-        'form': form,
-        'product': product,
-    }
+    product.delete()
+    messages.success(request, 'Product Removed')
+    return redirect(reverse('products'))
