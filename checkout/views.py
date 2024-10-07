8c7c1c5ed5067aea4b960747230f116fbf13f173
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-07 22:22:45 +0000

    Added Stripe elements and basic styling

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q7GNXGHjt7sxbbVp3cfjQl6uuEFQq45HeGfFRwLxl3gn3eEqNZnuwMqzp7jBYExJenbAIeT7K5cZYnGjjoJYbWt00RBdPMnWl',
+        'client_secret': 'test client secret',
 
     }
 
