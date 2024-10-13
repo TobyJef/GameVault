6dd64d4237b8b6bb4d3f90e3c0bec7e819bfec43
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-13 14:53:43 +0000

    add webhooks.py file and stripe wh secret to settings and gitpod enviroment variables

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,5 +11,21 @@ class StripeWH_Handler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
+            content=f'Unhandled webhook recieved {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeded(self, event):
+        """
+        Handle the payment_intent.succeded webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook recieved {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
             content=f'Webhook recieved {event["type"]}',
             status=200)
---
+++ checkout/webhooks.py
@@ -0,0 +1,50 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """Listen to webhooks from Stripe"""
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    # Get webhook data and verify its signature
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid Payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerficationError as e:
+        #  Invalid Signature
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
+        
+    return HttpResponse(status=400)
+
+    # Handle the event
+    if event.type == 'payment_intent.succeded':
+        payment_intent = event.data.object # Contains a stripe.PaymentIntent
+        print ('PaymentIntent was successful')
+    elif event.type == 'payment_method.attatched':
+        payment_method = event.data.object # Contains a stripe.PaymentMethod
+        print ('PaymentMethod was attatched to a customer')
+    # ... handle other event types
+    else:
+        # Unexpected event type
+        return HttpResponse(status=400)
+
+    return HttpRespons(status=200)
