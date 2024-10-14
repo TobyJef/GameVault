dcd1fdd83c36e6417fcce50a5d52af1da2bdaaf0
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-14 09:08:12 +0000

    created user profile app and migrated userprofile model

--- checkout/models.py
+++ checkout/models.py
@@ -7,21 +7,25 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
+
 
 # Create your models here.
 class Order(models.Model):
     order_number = models.CharField(max_length=35, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                null=True, blank=True, related_name='orders')
     title = models.CharField(max_length=10, null=True, blank=True)
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=255, null=False, blank=False)
     contact_number = models.CharField(max_length=20, null=True, blank=True)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
-    post_zipcode = models.CharField(max_length=20, null=False, blank=False)
-    town_or_city = models.CharField(max_length=50, null=False, blank=False)
     street_address_1 = models.CharField(max_length=100, null=False, blank=False)
     street_address_2 = models.CharField(max_length=80, null=False, blank=True)
+    town_or_city = models.CharField(max_length=50, null=False, blank=False)
+    post_zipcode = models.CharField(max_length=20, null=False, blank=False)
     county_or_state = models.CharField(max_length=80, null=True, blank=True)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add = True)
     delivery_type = models.CharField(max_length=32, null=False, blank=False)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
