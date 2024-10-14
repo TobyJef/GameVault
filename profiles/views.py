cf293a32f454824e6ee4c7464eb14dcc954845ed
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-14 09:23:14 +0000

    created user profile views, url and user profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    """ Displays the User's profile """
+    template ='profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
+
