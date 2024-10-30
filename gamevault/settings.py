4c447e1410a2eb32d102281bc872a7b4ea15b2ff
Toby Jefferies <toby_jefferies@hotmail.com>
2024-10-30 10:50:45 +0000

    create new superuser

--- gamevault/settings.py
+++ gamevault/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-#SECRET_KEY = 'django-insecure-^s7=368#hk00m40)v5nkh160l)zwflkarmtyv06lfmvf049_(u'
+#SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-^s7=368#hk00m40)v5nkh160l)zwflkarmtyv06lfmvf049_(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #Set to False on Deployment
